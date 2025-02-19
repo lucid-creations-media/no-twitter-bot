03b48c0513e84e67f4e3262151207bcfc9c41446
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2025-02-19 18:26:31 -0500

    Fix welcome command

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -6,11 +6,9 @@ const composer = new Composer<Context>();
 
 const feature = composer.chatType("private");
 
-feature.hears("start", logHandle("command-start"), ctx => {
+feature.command("start", logHandle("command-start"), ctx => {
   return ctx.reply(
-    ctx.t(
-      "Welcome! I am a bot created by Lucid for Lucid Creations media groups. I am designed to delete any Twitter/X links and reformatting services within groups. By default I only work with whitelisted group IDs. You can fork me from this link: https://github\\.com/lucid\\-creations\\-media/no\\-twitter\\-bot and deploy me for use in your own groups!"
-    ),
+    "Welcome! I am a bot created by Lucid for Lucid Creations media groups. I am designed to delete any Twitter/X links and reformatting services within groups. By default I only work with whitelisted group IDs. You can fork me from this link: https://github\\.com/lucid\\-creations\\-media/no\\-twitter\\-bot and deploy me for use in your own groups!",
     { parse_mode: "MarkdownV2" }
   );
 });
