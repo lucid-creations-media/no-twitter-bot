eebbb677053ac11d269da733d221e085d9c27437
Lucid Kobold <72232219+LucidKobold@users.noreply.github.com>
2025-02-19 18:34:52 -0500

    Fix welcome command

--- src/bot/features/admin.ts
+++ src/bot/features/admin.ts
@@ -1,15 +1,14 @@
 import { chatAction } from "@grammyjs/auto-chat-action";
 import { Composer } from "grammy";
 import type { Context } from "#root/bot/context.js";
-import { isAdmin } from "#root/bot/filters/is-admin.js";
+// import { isAdmin } from "#root/bot/filters/is-admin.js";
 import { setCommandsHandler } from "#root/bot/handlers/commands/setcommands.js";
 import { logHandle } from "#root/bot/helpers/logging.js";
 
 const composer = new Composer<Context>();
 
-const feature = composer
-  .chatType("private")
-  .filter(ctx => isAdmin(ctx.config.botAdmins)(ctx));
+const feature = composer.chatType("private");
+// .filter(ctx => isAdmin(ctx.config.botAdmins)(ctx));
 
 feature.command(
   "setcommands",
